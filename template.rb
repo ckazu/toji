a20233a79d2da3104484f594bdc26f14477d1455
ckazu <ckazuyuki@gmail.com>
2013-10-26 13:42:56 +0900

    add Gemfile for devlopment(or test)

--- template.rb
+++ template.rb
@@ -1,3 +1,8 @@
+def commit(message)
+  git add: '.'
+  git commit: "-m '#{message}'"
+end
+
 # first commit
 file 'README.md', 'README'
 git :init
@@ -6,3 +11,33 @@ git commit: "-m 'initial commit'"
 
 # commit initial rails files
 run "rm README.rdoc"
+
+commit "rails new"
+
+# Gemfiles
+gem_group :development, :test do
+  # rspec
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
+  gem 'ffaker'
+  # auto run
+  gem 'guard-rspec'
+  gem 'spring'
+  # ci
+  gem 'coveralls', require: false
+  # server
+  gem 'puma'
+  # debug
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+run 'bundle install'
+
+generate 'rspec:install'
+
+run 'bundle exec guard init rspec'
+
+commit "add development Gemfiles"
